c161a7197280405378720f7d4c827147b3682b05
louvivien <vivien.richaud@gmail.com>
2023-05-06 00:12:13 +0200

    error handling fix

--- app.py
+++ app.py
@@ -7,8 +7,12 @@ app = Flask(__name__)
 @app.route('/generate', methods=['POST'])
 def generate():
     input_text = request.form['input_text']
-    make_changes_and_push(input_text)
-    return jsonify({"status": "success"})
+    try:
+        make_changes_and_push(input_text)
+        return jsonify({"status": "success", "message": "HTML content generated and pushed to GitHub successfully!"})
+    except Exception as e:
+        print(e)
+        return jsonify({"status": "error", "message": "An error occurred. Please try again."})
 
 @app.route("/", methods=["GET", "POST"])
 def index():
