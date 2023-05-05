d6338459c35f161645476a45ffd9a7e02113a00e
louvivien <vivien.richaud@gmail.com>
2023-05-06 01:21:32 +0200

    sucess msg fix

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def generate():
     input_text = request.form['input_text']
     try:
         make_changes_and_push(input_text)
-        return jsonify({"status": "success", "message": "HTML content generated and pushed to GitHub successfully!"})
+        return jsonify({"status": "success", "message": "HTML content generated and pushed to GitHub successfully!<br>The content has been deployed on Vercel. <br><br>GitHub Repository: <a href='https://github.com/Louvivien/test' target='_blank'>https://github.com/Louvivien/test</a> <br>Vercel URL: <a href='https://test-louvivien.vercel.app/' target='_blank'>https://test-louvivien.vercel.app/</a>"})
     except Exception as e:
         print(e)
         return jsonify({"status": "error", "message": "An error occurred. Please try again."})
@@ -24,7 +24,7 @@ def index():
 
 @app.route("/success")
 def success():
-    return "HTML content generated and pushed to GitHub successfully! "
+    return "HTML content generated and pushed to GitHub successfully!<br>The content has been deployed on Vercel. <br><br>GitHub Repository: <a href='https://github.com/Louvivien/test' target='_blank'>https://github.com/Louvivien/test</a> <br>Vercel URL: <a href='https://test-louvivien.vercel.app/' target='_blank'>https://test-louvivien.vercel.app/</a>"
 
 if __name__ == "__main__":
     app.run(debug=True)
